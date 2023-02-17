bc9948b15442a8de40741d6ef50c9c8c4e09b6b4
Oleg <kornienko.0leg777@gmail.com>
2023-02-18 00:29:58 +0200

    add navbar

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css'
+    ],
+    build: {
+        transpile: ['primevue']
+    },
     components: [
         {
             path: '~/pages/admin/components',
